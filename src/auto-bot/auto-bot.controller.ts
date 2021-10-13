c512e19bf17a910d2af56b5e90450af72e2b1ccd
soonduck <ckdev301509@gmail.com>
2021-10-14 01:09:53 +0900

    1014

--- src/auto-bot/auto-bot.controller.ts
+++ src/auto-bot/auto-bot.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AutoBotService } from './auto-bot.service';
 
 @Controller('auto-bot')
-export class AutoBotController {}
+export class AutoBotController {
+  constructor(private readonly autoBotService: AutoBotService) {}
+  @Get('/log')
+  async getLog() {
+    console.log(this.autoBotService.getLog());
+    return this.autoBotService.getLog();
+  }
+}
--- src/auto-bot/auto-bot.service.ts
+++ src/auto-bot/auto-bot.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
 
 @Injectable()
-export class AutoBotService {}
+export class AutoBotService {
+  //lastSeenId
+  getLastSeenId() {
+    return fs.readFileSync('./src/auto-bot/log/last-seen-id.txt');
+  }
+  updateLastSeenId(id) {
+    fs.writeFileSync('./src/auto-bot/log/last-seen-id.txt', id);
+  }
+  //Log
+  getLog() {
+    return fs.readFileSync('./src/auto-bot/log/log.txt', 'utf-8');
+  }
+  updateLog(log) {
+    const oldLog = fs.readFileSync('./src/auto-bot/log/log.txt', 'utf-8');
+    fs.appendFileSync('./src/auto-bot/log/log.txt', ' \n' + log);
+    console.log(oldLog);
+  }
+}
